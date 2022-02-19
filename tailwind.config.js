0095af9d6c05856bf4bfce651e4e87ba5eee324b
Sujay Parkhe <sujay@Sujays-MacBook-Air.local>
2022-02-19 15:50:08 +0530

    first step of creating a website

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "roboto-mono": ["Roboto Mono", "monospace"]
+      }
+    },
+  },
+  plugins: [],
+}
